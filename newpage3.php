27888a6ba5046308429a4309b56bfba76d8ec652
Wadner Joseph <wjoseph@thefreshdiet.com>
2013-11-15 14:15:15 -0500

    New php

---
+++ newpage3.php
@@ -0,0 +1,15 @@
+
+<html>
+<body>
+Welcome <?php echo $_POST["name"]; ?><br>
+
+Your email address is: <?php echo $_POST["email"]; ?>
+<br>
+ <?php echo $_POST["priceSum"]; ?>
+.<br>
+
+<form name="form1" action="newpage2.php">
+<input type="submit" value="Go Back" />
+</form>
+</body>
+</html>
